2ff36a5ecea251ca83616f0a3fb850e99d3f7aff
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2021-08-10 12:52:52 +0200

    add advanced mathematical function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -27,6 +27,31 @@ function divideWith() {
   return outputResult(currentResult, "");
 }
 
+function reset() {
+  currentResult = 0;
+  return outputResult(currentResult, "");
+}
+
+function powerOf() {
+  input = parseInt(userInput.value);
+
+  if (input === 0) {
+    return outputResult(1, "");
+  } else if (input === 1) {
+    return outputResult(currentResult, "");
+  }
+
+  multiplier = currentResult;
+  for (let index = 1; index < input; index++) {
+    currentResult = currentResult * multiplier;
+  }
+  return outputResult(currentResult, "");
+}
+
+function modulo() {
+  currentResult = currentResult % parseInt(userInput.value);
+}
+
 addBtn.addEventListener("click", addWith);
 subtractBtn.addEventListener("click", substractWith);
 multiplyBtn.addEventListener("click", multiplyWith);
