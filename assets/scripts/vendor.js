2179d0b83c3a4a4c46b4bb77c6ad88c9902272b1
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2021-08-10 19:07:12 +0200

    make result text disappear upon initial value

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,13 +1,22 @@
-const userInput = document.getElementById('input-number');
-const addBtn = document.getElementById('btn-add');
-const subtractBtn = document.getElementById('btn-subtract');
-const multiplyBtn = document.getElementById('btn-multiply');
-const divideBtn = document.getElementById('btn-divide');
+const userInput = document.getElementById("input-number");
+const addBtn = document.getElementById("btn-add");
+const subtractBtn = document.getElementById("btn-subtract");
+const multiplyBtn = document.getElementById("btn-multiply");
+const divideBtn = document.getElementById("btn-divide");
 
-const currentResultOutput = document.getElementById('current-result');
-const currentCalculationOutput = document.getElementById('current-calculation');
+const resultTxt = document.getElementById("result-txt");
+const currentResultOutput = document.getElementById("current-result");
+const currentCalculationOutput = document.getElementById("current-calculation");
 
 function outputResult(result, text) {
-  currentResultOutput.textContent = result;
-  currentCalculationOutput.textContent = text;
+  if (result === 0) {
+    resultTxt.style.display = "none";
+    currentResultOutput.style.display = "none";
+    currentCalculationOutput.textContent = text;
+  } else {
+    resultTxt.style.display = "inline";
+    currentResultOutput.style.display = "inline";
+    currentResultOutput.textContent = result;
+    currentCalculationOutput.textContent = text;
+  }
 }
