ad98dd23c8f989cba14a03cf59ad89c981e81ee9
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2021-08-10 12:34:51 +0200

    add mathematical operations

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,33 @@
-let currentResult = 0;
+const defaultResult = parseInt(currentResultOutput.textContent);
+let currentResult = defaultResult;
 
-let calcDesc = currentResult + " + 10";
+let calcDesc = `${defaultResult} + 10`;
+let errorMsg = "An Error has occured!";
 // alert(currentResult);
 
-currentResult = currentResult + 10;
-
 outputResult(currentResult, calcDesc);
+
+function addWith() {
+  currentResult = currentResult + parseInt(userInput.value);
+  return outputResult(currentResult, "");
+}
+
+function substractWith() {
+  currentResult = currentResult - parseInt(userInput.value);
+  return outputResult(currentResult, "");
+}
+
+function multiplyWith() {
+  currentResult = currentResult * parseInt(userInput.value);
+  return outputResult(currentResult, "");
+}
+
+function divideWith() {
+  currentResult = currentResult / parseInt(userInput.value);
+  return outputResult(currentResult, "");
+}
+
+addBtn.addEventListener("click", addWith);
+subtractBtn.addEventListener("click", substractWith);
+multiplyBtn.addEventListener("click", multiplyWith);
+divideBtn.addEventListener("click", divideWith);
