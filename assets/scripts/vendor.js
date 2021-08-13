49bc2305199f17c4eb1e9301189145ef3fd37f30
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2021-08-13 15:53:31 +0200

    fix the result does not show after value is zero or upon resetting the value

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -4,12 +4,17 @@ const subtractBtn = document.getElementById("btn-subtract");
 const multiplyBtn = document.getElementById("btn-multiply");
 const divideBtn = document.getElementById("btn-divide");
 
+const powerOfBtn = document.getElementById("btn-powerOf");
+const moduloBtn = document.getElementById("btn-modulo");
+
+const resetBtn = document.getElementById("btn-reset");
+
 const resultTxt = document.getElementById("result-txt");
 const currentResultOutput = document.getElementById("current-result");
 const currentCalculationOutput = document.getElementById("current-calculation");
 
 function outputResult(result, text) {
-  if (result === 0) {
+  if (result === 0 && histories.length < 1) {
     resultTxt.style.display = "none";
     currentResultOutput.style.display = "none";
     currentCalculationOutput.textContent = text;
