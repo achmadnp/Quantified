cd5a986b6243f1950864d2dbf503d55089808024
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2021-08-13 15:52:46 +0200

    add functionalities to newly added button and reduce redundancy

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,12 +2,12 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 let calcDesc = `Initial value: ${defaultResult}`;
-let history = [];
+let histories = [];
 
 outputResult(currentResult, calcDesc);
 
 function addToCalcHistory(desc) {
-  history.push(`${desc} = ${currentResult}`);
+  histories.push(`${desc} = ${currentResult}`);
 }
 
 function getUserInput() {
@@ -19,69 +19,88 @@ function calcOperator(operator, resultBefore, calcNum) {
     Math.round(1000000000 * resultBefore) / 1000000000
   } ${operator} ${calcNum}`;
   addToCalcHistory(desc);
-  console.log(history);
+  console.log(histories);
   outputResult(Math.round(1000000000 * currentResult) / 1000000000, desc);
 }
 
-function addWith() {
+function calcDecider(calcType) {
   const enteredNum = getUserInput();
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNum;
-  calcOperator("+", initialResult, enteredNum);
+  let mathOperator;
+  if (calcType === "ADD") {
+    currentResult = currentResult + enteredNum;
+    mathOperator = "+";
+  } else if (calcType === "SUBSTRACT") {
+    currentResult = currentResult - enteredNum;
+    mathOperator = "-";
+  } else if (calcType === "MULTIPLY") {
+    currentResult = currentResult * enteredNum;
+    mathOperator = "*";
+  } else if (calcType === "DIVIDE") {
+    currentResult = currentResult / enteredNum;
+    mathOperator = "/";
+  } else if (calcType === "POWER-OF") {
+    const multiplier = currentResult;
+    mathOperator = "^";
+
+    if (enteredNum === 0) {
+      currentResult = 1;
+      return calcOperator(mathOperator, initialResult, enteredNum);
+    }
+    for (let index = 1; index < enteredNum; index++) {
+      currentResult = currentResult * multiplier;
+    }
+    calcOperator(mathOperator, initialResult, enteredNum);
+  } else if (calcType === "MODULO") {
+    mathOperator = "%";
+    currentResult = currentResult % enteredNum;
+    calcOperator(mathOperator, initialResult, enteredNum);
+  } else {
+    console.error("FAILED TO RETRIEVE OPERATOR");
+    return;
+  }
+  calcOperator(mathOperator, initialResult, enteredNum);
+}
+
+function addWith() {
+  calcDecider("ADD");
 }
 
 function substractWith() {
-  const enteredNum = getUserInput();
-  const initialResult = currentResult;
-  currentResult = currentResult - enteredNum;
-  calcOperator("-", initialResult, enteredNum);
+  calcDecider("SUBSTRACT");
 }
 
 function multiplyWith() {
-  const enteredNum = getUserInput();
-  const initialResult = currentResult;
-  currentResult = currentResult * enteredNum;
-  calcOperator("*", initialResult, enteredNum);
+  calcDecider("MULTIPLY");
 }
 
 function divideWith() {
-  const enteredNum = getUserInput();
-  const initialResult = currentResult;
-  currentResult = currentResult / enteredNum;
-  calcOperator("/", initialResult, enteredNum);
+  // const enteredNum = getUserInput();
+  // const initialResult = currentResult;
+  // currentResult = currentResult / enteredNum;
+  // calcOperator("/", initialResult, enteredNum);
+  calcDecider("DIVIDE");
 }
 
 function reset() {
-  const desc = "value has been result to 0";
+  const desc = "value has been result to 0 and logs have been cleared";
   currentResult = 0;
+  histories = [];
   return outputResult(currentResult, desc);
 }
 
 function powerOf() {
-  const desc = `${currentResult} ^ ${userInput.value}`;
-
-  input = parseInt(userInput.value);
-
-  if (input === 0) {
-    return outputResult(1, "");
-  } else if (input === 1) {
-    return outputResult(currentResult, "");
-  }
-
-  multiplier = currentResult;
-  for (let index = 1; index < input; index++) {
-    currentResult = currentResult * multiplier;
-  }
-  return outputResult(currentResult, "");
+  calcDecider("POWER-OF");
 }
 
 function modulo() {
-  const desc = `${currentResult} % ${userInput.value}`;
-  currentResult = currentResult % parseInt(userInput.value);
-  return outputResult(currentResult, desc);
+  calcDecider("MODULO");
 }
 
 addBtn.addEventListener("click", addWith);
 subtractBtn.addEventListener("click", substractWith);
 multiplyBtn.addEventListener("click", multiplyWith);
 divideBtn.addEventListener("click", divideWith);
+resetBtn.addEventListener("click", reset);
+powerOfBtn.addEventListener("click", powerOf);
+moduloBtn.addEventListener("click", modulo);
